37415e14364bfa47717210cf1680bb3c30762c5a
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2022-11-26 02:56:04 +0600

    firts fetch data in server
    firts fetch data in server

---
+++ firts fetch data in server/main/java/com/example/firstfetdata/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.firstfetdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.texttext2);
+        progressBar=findViewById(R.id.progressBar);
+
+        //
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://rabbylearnandfun.000webhostapp.com/apps/hello.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is \n " +response);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        queue.add(stringRequest);
+
+    }
+}
